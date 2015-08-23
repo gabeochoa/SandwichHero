e3509697fd7ac38c9f03258f5831793b851b9b19
Gabriel Ochoa <gochoa1@bingahamton.edu>
2015-08-23 00:44:38 -0400

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+
+	public float gravity = 20;
+	public float speed = 2;
+	public float accel = 64;
+	public float jumpH = 4;
+	
+	private float curspeed;
+	private float tarspeed;
+	
+	private Vector2 toMove;
+	
+	private PlayerPhysics playerPhys;
+	// Use this for initialization
+	void Start () {
+		playerPhys = GetComponent<PlayerPhysics>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tarspeed = Input.GetAxisRaw("Horizontal") * speed;
+		curspeed = IncrementTowards(curspeed, tarspeed, accel);
+		
+		if(playerPhys.grounded)
+		{
+			toMove.y = 0;
+			if(Input.GetAxisRaw("Vertical") != 0 || Input.GetButton("Jump"))
+			{
+				toMove.y = jumpH;
+			}			
+		}
+		
+		toMove.x = curspeed;
+		toMove.y -= gravity * Time.deltaTime;
+		playerPhys.Move(toMove * Time.deltaTime);
+	}
+	
+	private float IncrementTowards(float c, float t, float tween)
+	{
+		if(c == t)
+		{
+			return c;
+		}
+		else
+		{
+			float dir = Mathf.Sign(t-c);
+			c += tween * Time.deltaTime * dir;
+			return (dir == Mathf.Sign(t-c))? c: t;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+	
+	private BoxCollider coll;
+	private Vector3 size;
+	private Vector3 pos;
+	
+	[HideInInspector]
+	public bool grounded;
+
+	private Ray ray;
+	private RaycastHit hit;
+	private float bubble = .005f;
+	public LayerMask collidesWith;
+
+	void Start()
+	{
+		coll = GetComponent<BoxCollider>();
+		size = coll.size;
+		pos = coll.center;
+	}
+
+	public void Move(Vector2 moveamt)
+	{
+		float dX = moveamt.x;
+		float dY = moveamt.y;
+		Vector2 p = transform.position;
+		float dir = Mathf.Sign(dY);		
+		grounded = false;
+		for (int i=0; i<3; i++)
+		{
+			float x = (p.x + pos.x + size.x/2) + size.x/2 *i;
+			float y = p.y + pos.y + size.y/2 * dir;
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(0, dir));
+			if(Physics.Raycast(ray, out hit, Mathf.Abs(dY), collidesWith))
+			{
+				float dst = Vector3.Distance(ray.origin, hit.point);
+				if(dst > bubble)
+				{
+					dY = dst*dir + bubble;
+				}			
+				else
+				{
+					dY = 0;
+				}
+				
+				grounded = true;
+				break;
+			}
+		}
+		transform.Translate(new Vector2(dX, dY));
+	}
+}
